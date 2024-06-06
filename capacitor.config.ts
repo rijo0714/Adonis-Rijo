296e11c9c6aff12520285e2724342389dd6f3c1b
rijo0714 <152038901+rijo0714@users.noreply.github.com>
2024-06-05 22:22:38 -0400

    Proyecto Parcial 1

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Parcial1_movil',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
